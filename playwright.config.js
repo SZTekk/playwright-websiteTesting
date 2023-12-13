d3f638fcaff55bea0592fa5c251e7b0526532724
Fiverr123456789 <Saeztech@gmail.com>
2023-12-13 12:27:32 +0600

    configuration

--- playwright.config.js
+++ playwright.config.js
@@ -4,3 +4,23 @@ const { defineConfig, devices } = require('@playwright/test');
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
+module.exports = defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  timeout: 40000,
+  use: {
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chrome',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
